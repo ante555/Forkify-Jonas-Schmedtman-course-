e796892b00a7a90bddef97befca4e716b2d88841
ante555 <ante.stipic93@gmail.com>
2021-09-06 18:13:14 +0200

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '717fa1df-9f55-4f03-96d8-d2386a3ee5c1';
+export const MODAL_CLOSE_SEC = 2.5;
